0b966fd99e7ba81178a52a9606581d879f0d5b8f
Baruch Phillips <baruchphillips@gmail.com>
2019-10-31 21:13:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     return programmer_hash[:dennis_ritchie][:languages].split
+     programmer_hash[:dennis_ritchie][:languages].split
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
