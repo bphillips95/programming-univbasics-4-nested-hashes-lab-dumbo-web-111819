ba2ce7883779a74437d99d60be3deefffaa0f29d
Baruch Phillips <baruchphillips@gmail.com>
2019-10-31 21:28:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages]["Assembly"].shift 
+    programmer_hash[:dennis_ritchie][:languages]["Assembly"].push
 return programmer_hash
 end
